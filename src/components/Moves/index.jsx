8db394a573d8058c490755bf814500b28aabe074
C3-PeeOh <damyan.b.genov@gmail.com>
2024-03-25 21:53:04 +0200

    Moves uses a hash table now

--- src/components/Moves/index.jsx
+++ src/components/Moves/index.jsx
@@ -1,30 +1,27 @@
-import { calculateWinner } from "../../utils/calculate-winner"
-import { getSquareCoordinates } from "../../utils/get-square-coordinates"
+import { calculateWinner } from "../../utils/calculate-winner";
+import { getSquareCoordinates } from "../../utils/get-square-coordinates";
 
 const Moves = ({ sortedHistory, xIsNext, jumpTo, lastMove, sortAscending }) => {
+    const descriptions = {
+        [sortedHistory.length - 1]: () => {
+            const { row, column } = getSquareCoordinates(lastMove);
+            let description = `You are at move #${sortedHistory.length - 1}. ${!xIsNext ? "X" : "O"} placed on Row: ${row} Column: ${column}`;
+            const winner = calculateWinner(sortedHistory[sortedHistory.length - 1]);
+            if (winner.symbol) {
+                description += " WINNER: " + winner.symbol;
+            } else if (sortedHistory.length - 1 === 9) {
+                description += " DRAW";
+            }
+            return description;
+        },
+        0: () => "Go to game start",
+    };
+
     return (
         <ol>
             {sortedHistory.map((squares, move) => {
                 const moveOrder = sortAscending ? move : sortedHistory.length - 1 - move;
-                let description;
-                let winner = calculateWinner(squares);
-
-                switch (moveOrder) {
-                    case sortedHistory.length - 1:
-                        const { row, column } = getSquareCoordinates(lastMove);
-                        description = `You are at move #${moveOrder}. ${!xIsNext ? "X" : "O"} placed on Row: ${row} Column: ${column}`;
-                        if (winner.symbol) {
-                            description += " WINNER: " + winner.symbol;
-                        } else if (moveOrder === 9) {
-                            description += " DRAW";
-                        }
-                        break;
-                    case 0:
-                        description = "Go to game start";
-                        break;
-                    default:
-                        description = "Go to move #" + moveOrder;
-                }
+                const description = descriptions[moveOrder] ? descriptions[moveOrder]() : `Go to move #${moveOrder}`;
 
                 return (
                     <li key={move}>
